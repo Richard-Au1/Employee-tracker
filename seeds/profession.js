a4c7e1fedd6d4c096a5abde06a89483c204ae8e4
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-11-29 23:44:26 -0700

    updated seed files

---
+++ seeds/profession.js
@@ -0,0 +1,12 @@
+const sequelize = require("../connection");
+const Profession = require("../models/profession");
+
+const professionSeedData = require("./professionSeedData.json");
+
+const seedProfessionData = async () => {
+  await sequelize.sync({ force: true });
+  const profession = await Profession.bulkCreate(professionSeedData);
+  process.exit(0);
+};
+
+seedProfessionData();
