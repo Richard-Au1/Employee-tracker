a94ed07e49a28f8062230ecb2099825d1d86a784
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-11-29 20:43:46 -0700

    connected the models together to create a table with each value

--- models/department.js
+++ models/department.js
@@ -1,2 +1,27 @@
 //the structure for department section of the table
-const {Model, Datatypes} = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../connection");
+
+class Department extends Model {}
+
+Department.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Department",
+  }
+);
+
+module.exports = Department;
